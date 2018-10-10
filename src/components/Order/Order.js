8b39ea4c90d939444593ee778f24a57a587b49ec
Doug Popadince <gsmatth@icloud.com>
2018-10-10 09:52:27 -0700

    added Orders and Order components

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -2,11 +2,28 @@ import React from 'react';
 import styles from './Order.css';
 
 const Order = (props) => {
+  
+    /**    we want to create an array of objects from the props.ingredients object.  */
 
+  const ingredients = [];
+  
+  for (let ingredientName in props.ingredients){
+    ingredients.push({
+      name: ingredientName, 
+      amount: props.ingredients[ingredientName]});
+  }
+  
+  console.log('[Order.js] newly created ingredients Array: ',ingredients);
+
+  const ingredientOutput = ingredients.map(ingredient => {
+    return (
+      <span key={ingredient.name}>{`${ingredient.name}: ${ingredient.amount}`}</span>
+    )
+  })
   return (
     <div className={styles.Order}>
-      <p>Ingredients: {props.ingredient} </p>
-      <p>Price: <strong>USD {props.price}</strong></p>
+      <p>Ingredients: {ingredientOutput} </p>
+      <p>Price: <strong>USD {props.price.toFixed(2)}</strong></p>
     </div>
   )
 }
--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -1,36 +1,60 @@
 import React, { Component } from 'react';
 import axios from '../../axios-orders';
 import Order from '../../components/Order/Order';
+import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
 
 class Orders extends Component {
   state = {
-    orders: null
+    orders: [],
+    loading: true
   }
 
+  /**
+   * below we convert the javascript object returned from get call, to an array "fetchedOrders".  for (var variable in objectName) {statement}
+   *  var myObj = {a: 1, b: 2, c: 3}, myKeys = [];
+   *  for (let property in myObj) {
+   *    myKeys.push(property);
+   *  }
+   * myKeys; //['a','b','c'];
+   */
   componentDidMount () {
-    axios.get('https://react-my-burger-58e7d.firebaseio.com/orders.json')
+    axios.get('/orders.json')
       .then(response => {
-        this.setState({orders: response})
-      });
+        console.log('[Orders.js] response.data object in componentDidMount: ', response.data);
+        const fetchedOrders = [];
+        for(let key in response.data){
+          fetchedOrders.push(
+            {...response.data[key],
+              id: key});
+        }
+        console.log('[Orders.js] fetchedOrders array after get: ', fetchedOrders);
+        this.setState({
+          loading: false,
+          orders: fetchedOrders
+        })
+      })
+      .then(() => {
+        console.log('[Orders.js] state.orders after update in componentDidMount: ', this.state.orders);
+      })
+          .catch(err => {
+            this.setState({loading: false})
+          })
+
   }
 
-  render() {
-    let posts =  '';
-    posts = this.state.orders.map((order) => {
-      return (
-        <Order
-          key={order.id}
-          ingredients={order.ingredients}
-          price={order.totalPrice} />
-      )
-    })
-
+  render () {
+    console.log('[Orders.js] state.orders in render: ', this.state.orders);
     return (
-      <div>
-        {posts}
-      </div>
+        <div>
+            {this.state.orders.map(order => (
+                <Order 
+                    key={order.id}
+                    ingredients={order.ingredients}
+                    price={+order.totalPrice} />
+            ))}
+        </div>
     );
-  };
+}
 }
 
-export default Orders;
+export default withErrorHandler(Orders, axios);
