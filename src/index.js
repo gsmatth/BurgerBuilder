627c85369ab94b5d841f4cfbfeb433938c9d5d37
Doug Popadince <gsmatth@icloud.com>
2018-10-23 12:20:11 -0700

    enabled thunk and redux-dev-tools in index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import{ BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
 import burgerReducer from './store/reducers/burgerReducer';
 import checkoutReducer from './store/reducers/checkoutReducer';
 import './index.css';
@@ -14,7 +15,22 @@ const rootReducer = combineReducers({
   checkout: checkoutReducer
 })
 
-const store = createStore(rootReducer);
+const logger = store => {
+  return next => {
+    console.log('[Middleware] value of next: ', next);
+    return action => {
+      console.log('[Middleware] the action being dispatched: ', action);
+      const result = next(action);
+      console.log('[Middleware] next state: ', store.getState());
+      return result;
+    }
+  }
+}
+
+//allows use of redux tool browser plugin https://github.com/zalmoxisus/redux-devtools-extension#usage
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(rootReducer, /* preloadedState, */ composeEnhancers(applyMiddleware(logger, thunk)));
 
 ReactDOM.render(
   <Provider store={store}>
